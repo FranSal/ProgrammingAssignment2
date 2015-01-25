54c47df4158b73c1cd0df95917f59671f1e8ec6e
FranSal <rubycscf@me.com>
2015-01-25 19:05:46 +0100

    Vers 1.01
     changes in the comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,14 +3,15 @@
 ## that allow to store its value and inverse matrix.
 ##  set  first checks if the X argument is DIFFERENT ( !identical()) to a potentially stored value. 
 ## if it's diferent it caches the new value on X and sets the iM( inverse matrix) to NULL if it's identical
-## then it does nothing
+## then it does nothing. This is the function to use to modify the matrix, once the cached object has been created
+## by makeCacheMatrix(X)
 
 ## iM is the inverse matrix cache
 ## X is the cache to X argument
 ## get delivers X value back from the  X cache
 ## setinv stores the inverse matrix ( that is its argument X) to the iM cache
 ## getinv takes the cached value and returns it.
-## MakeCacheMatrix is the maker of a cached matrix object. To modify the matriz object itself
+## MakeCacheMatrix is the maker of a cached matrix object. To modify the matrix object itself
 ## X$set on the cached object  must used. this function checks for equiality. Using the naked 
 ## makeCacheMatrix will reset the cached object to another object, erasing its cache.
 
